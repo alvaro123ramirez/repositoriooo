b69142a6ba04f9c191b80bc18daaa5cccb01113e
Alvaro Ramirez <20214055@aloe.ulima.edu.pe>
2024-11-21 22:06:37 -0500

    actualizadoo

--- src/components/moleculas/header/ShowRecomendados.jsx
+++ src/components/moleculas/header/ShowRecomendados.jsx
@@ -1,6 +1,18 @@
 const ShowRecomendados = () => {
-    return(
-        <h1>as</h1>
+    return (
+        <>
+        <h2 className="title-recomendados">Recomendados</h2>
+        <div className="container-padre-recomendados">
+            <div className="container-recomendados">
+                <div><img src="/src/images/imagen-recomendados.png" alt="" /></div>
+                <div><img src="/src/images/imagen-recomendados.png" alt="" /></div>
+                <div><img src="/src/images/imagen-recomendados.png" alt="" /></div>
+                <div><img src="/src/images/imagen-recomendados.png" alt="" /></div>
+                <div><img src="/src/images/imagen-recomendados.png" alt="" /></div>
+                <div><img src="/src/images/imagen-recomendados.png" alt="" /></div>
+            </div>
+        </div>
+        </>
     )
 }
 export default ShowRecomendados
--- src/components/organismos/MainFooter.jsx
+++ src/components/organismos/MainFooter.jsx
@@ -1,20 +1,42 @@
 const MainFooter = () => {
-    return(
-        <footer>
-            <section>
+    return (
+        <footer className="container-padre-footer">
+            <section className="container-footer">
                 <div>
                     <h3>Contactanos</h3>
+                    <p>Chatea con nosotros
+                        Te atendemos las 24 horas</p>
+                    <p>Escríbenos
+                        Estamos para ayudarte
+                        sac-online@oechsle.pe</p>
+                    <p>Llámanos al 01 619-4801
+                        De lunes a domingo de 8:00 am a 8:00 pm</p>
+                    <p>Visítanos
+                        Revisa aquí nuestros horarios y tiendas disponibles a nivel nacional Ver tiendas</p>
                 </div>
                 <div>
                     <h3>Secciones destacadas</h3>
+                    <p>¿Cómo comprar en Oechsle?</p>
+                    <p>Cambios y devoluciones</p>
+                    <p>Despacho a Domicilio</p>
+                    <p>Mis Pedidos</p>
+                    <p>Devoluciones sin salir de casa</p>
                 </div>
                 <div>
                     <h3>Servicio al cliente</h3>
+                    <p>Términos y Condiciones Generales</p>
+                    <p>Términos y Condiciones de Promociones Comerciales</p>
+                    <p>Política de tratamiento de datos personales para finalidad adicional</p>
+                    <p>Política de privacidad comercio electrónico, envío, armado e instalación de productos</p>
                 </div>
                 <div>
                     <h3>
                         Sobre Oeschle
                     </h3>
+                    <p>Nosotros</p>
+                    <p>Nuestras tiendas</p>
+                    <p>Trabaja con nosotros</p>
+                    <p>Ventas institucionales</p>
                 </div>
             </section>
         </footer>
