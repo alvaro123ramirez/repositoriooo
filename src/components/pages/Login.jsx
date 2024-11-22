9995c37be95af8121e29ec8dc7b2e7f2addf9c50
Alvaro Ramirez <20214055@aloe.ulima.edu.pe>
2024-11-21 22:41:14 -0500

    actualizado otra vez

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -2,26 +2,36 @@ import { API_URL } from "../../constants/env"
 import axios from "axios"
 const Login = () => {
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault()
         alert("Formulario enviado")
 
         const data = {
-            email:e.target.email.value,
-            password:e.target.password.value
+            email: e.target.email.value,
+            password: e.target.password.value
         }
 
-        axios.post(`${API_URL}/users`, data)
-        .then(resp=>console.log(resp))
-        .catch(error=>console.log(error))
+        // axios.post(`${API_URL}/users`, data)
+        // .then(resp=>console.log(resp))
+        // .catch(error=>console.log(error))
     }
-    return(
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="email" placeholder="Correo electronico" required/>
-            <input type="password" name="password" placeholder="Contrasenia" required/>
-            <button type="submit">Ingresar</button>
-
-        </form>
+    return (
+        <div className="container-padre-formulario">
+            <form onSubmit={handleSubmit} className="container-formulario">
+                <h3 className="title-form">Iniciar sesion: </h3>
+                <div className="container-input-1-form">
+                    <label htmlFor="correo">Correo: </label><br />
+                    <input type="text" name="email" placeholder="Correo electronico" required className="input-1-form" id="correo" />
+                </div>
+                <div className="container-input-2-form">
+                    <label htmlFor="password">Contraseña: </label><br />
+                    <input type="password" name="password" placeholder="Contrasenia" required className="input-2-form" id="password" />
+                </div>
+                <div className="container-boton-form">
+                    <button type="submit" >Ingresar</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
