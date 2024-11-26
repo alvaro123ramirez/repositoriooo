6e3d90fc615ca501040a9fb1e017b79d707c9cfc
Alvaro Ramirez <20214055@aloe.ulima.edu.pe>
2024-11-26 18:31:30 -0500

    Acaulziado

--- src/components/moleculas/header/MainProductos.jsx
+++ src/components/moleculas/header/MainProductos.jsx
@@ -1,30 +1,15 @@
+import { Link } from "react-router-dom"
+
 const MainProductos = () => {
+
+    const listarProductos = productos.map( (prod, index) => <div key={index}><Link to="/detalles-producto"><img src={prod.url} alt="" /></Link> </div> )    
     return (
         <div className="container-principal-productos">
             <div className="container-parrafo-productos">
                 <p><b>Lo mas top</b> del Flash Sale</p>
             </div>
             <div className="container-productos"> 
-                <div className="imagen-1-producto"><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
-                <div><img src="src/images/imagen-producto-prueba.png" alt="" /></div>
+                {listarProductos}
             </div>
         </div>
     )
