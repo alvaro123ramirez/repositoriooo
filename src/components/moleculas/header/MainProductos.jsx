086a8def4f120f51b33585d8236a28d7b90a4230
Alvaro Ramirez <20214055@aloe.ulima.edu.pe>
2024-11-26 19:20:54 -0500

    Acaulziado

--- src/components/moleculas/header/MainProductos.jsx
+++ src/components/moleculas/header/MainProductos.jsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const MainProductos = () => {
+    
+    let navigate = useNavigate()
 
-    const listarProductos = productos.map( (prod, index) => <div key={index}><Link to="/detalles-producto"><img src={prod.url} alt="" /></Link> </div> )    
+    const listarProductos = productos.map( (prod, index) => <div key={index} onClick={()=>navigate(`/detalles-producto/${index}`)}><img src={prod.url} alt="" /></div>)    
     return (
         <div className="container-principal-productos">
             <div className="container-parrafo-productos">
                 <p><b>Lo mas top</b> del Flash Sale</p>
             </div>
             <div className="container-productos"> 
-                {listarProductos}
+                {listarProductos}  
             </div>
         </div>
     )
